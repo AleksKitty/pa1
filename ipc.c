26fdcd675a569b69977beab24ad935b1db376932
AleksKitty <alexandra.tochansky@gmail.com>
2020-06-14 23:34:03 +0300

    Not working right - problems with amount of open pipes

---
+++ ipc.c
@@ -0,0 +1,137 @@
+//
+// Created by alex on 13.06.2020.
+//
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include "ipc.h"
+
+typedef struct {
+    pid_t pid; // special id for processes
+    local_id localId; // id from ipc.h
+    int *pipe_read; // who we need to READ from
+    int *pipe_write; // who we need to WRITE into
+}  process;
+
+
+
+int number_of_processes;
+
+//------------------------------------------------------------------------------
+/** Send a message to the process specified by id.
+ *
+ * @param self    Any data structure implemented by students to perform I/O
+ * @param dst     ID of recepient
+ * @param msg     Message to send
+ *
+ * @return 0 on success, any non-zero value on error
+ */
+
+int send(void *self, local_id dst, const Message *msg) {
+    process *sender = self;
+
+    int fd = sender->pipe_write[dst];
+
+    if (write(fd, msg, sizeof(MessageHeader) + msg->s_header.s_payload_len) == -1) {
+        perror("Error\n");
+        return -1;
+    }
+
+//    printf("Sending : process %d sent to process %d message: %s\n", sender->localId, dst, msg->s_payload);
+
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+
+/** Send multicast message.
+ *
+ * Send msg to all other processes including parrent.
+ * Should stop on the first error.
+ *
+ * @param self    Any data structure implemented by students to perform I/O
+ * @param msg     Message to multicast.
+ *
+ * @return 0 on success, any non-zero value on error
+ */
+int send_multicast(void * self, const Message * msg) {
+    process *process = self;
+    for (int i = 0; i < number_of_processes; i++) {
+        if (i != process->localId) {
+
+            if (send(self, i, msg) == -1) {
+                printf("Send = -1");
+                return -1;
+            }
+        }
+    }
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+
+/** Receive a message from the process specified by id.
+ *
+ * Might block depending on IPC settings.
+ *
+ * @param self    Any data structure implemented by students to perform I/O
+ * @param from    ID of the process to receive message from
+ * @param msg     Message structure allocated by the caller
+ *
+ * @return 0 on success, any non-zero value on error
+ */
+int receive(void * self, local_id from, Message * msg) {
+    process * receiver = self;
+
+
+    int fd = receiver->pipe_read[from]; // where exactly we are sending!
+
+    if ((read(fd, &msg->s_header, sizeof(MessageHeader))) == -1) {
+        printf("Error!\n");
+        return -1;
+    }
+
+    if ((read(fd, &msg->s_payload, msg->s_header.s_payload_len)) == -1) {
+        printf("Error!\n");
+        return -1;
+    }
+
+    printf("Receiving : Process %d received from process %d message : %s\n", receiver->localId, from, (char *) &msg->s_payload);
+
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+
+/** Receive a message from any process.
+ *
+ * Receive a message from any process, in case of blocking I/O should be used
+ * with extra care to avoid deadlocks.
+ *
+ * @param self    Any data structure implemented by students to perform I/O
+ * @param msg     Message structure allocated by the caller
+ *
+ * @return 0 on success, any non-zero value on error
+ */
+int receive_any(void * self, Message * msg) {
+    process *process = self;
+    int index_start = 1;
+    if (process->localId == 0) { // it's parent   // you didnt send from 0 process!!!
+        index_start = 0;
+    }
+
+    for (int index_pipe_read = index_start; index_pipe_read < number_of_processes; index_pipe_read++) {
+        if (index_pipe_read != process->localId) {
+            int result = receive(self, index_pipe_read, msg);
+
+            if (result == -1) {
+                printf("Receive = -1\n");
+                return -1;
+            }
+        }
+    }
+
+    return 0;
+}
+
---
+++ main.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "ipc.h"
+#include "common.h"
+#include "pa1.h"
+
+extern int number_of_processes; // from input
+
+
+
+typedef struct {
+    pid_t pid; // special id for processes
+    local_id localId; // id from ipc.h
+    int *pipe_read; // who we need to READ from
+    int *pipe_write; // who we need to WRITE into
+}  process;
+
+
+
+static void create_pipes(process *array_of_processes, int *fd) {
+    printf("Creating pipes!\n");
+
+    FILE *pipe_log = fopen("pipes", "w"); // for writing into file
+    for (int i = 0; i < number_of_processes; i++) {
+        for (int j = 0; j < number_of_processes; j++) { // making pipes for everyone to everyone
+            // try to create new pipe
+
+            if (i != j) {
+                if (pipe(fd) < 0) { // fail
+                    printf("Can't create new pipe\n");
+                    exit(-1);
+                }
+                array_of_processes[j].pipe_read[i] = fd[0]; // j read from i
+                array_of_processes[i].pipe_write[j] = fd[1]; // i write into j
+            } else {
+                array_of_processes[j].pipe_read[i] = -1; // can't read from itself
+                array_of_processes[i].pipe_write[j] = -1; // can't write into itself
+                continue;
+            }
+
+            printf("Pipe (read %d, write %d) has OPENED\n", fd[0], fd[1]);
+            fprintf(pipe_log, "Pipe (read %d, write %d) has OPENED\n", fd[0], fd[1]);
+            //fflush(pipe_log);
+        }
+
+    }
+    fclose(pipe_log);
+}
+
+static void close_all_pipes(process array_of_processes[]) {
+    for (int i = 0; i < number_of_processes; i++) {
+        for (int j = 0; j < number_of_processes; j++) {
+            if (i != j) {
+                close(array_of_processes[j].pipe_read[j]);
+                close(array_of_processes[i].pipe_write[j]);
+            }
+        }
+    }
+}
+
+
+static void create_processes(process *array_of_processes) {
+    FILE *event_log = fopen(events_log, "w"); // for writing into file
+    printf("Creating processes:\n");
+
+    for (int i = 1; i < number_of_processes; i++) {
+        array_of_processes[i].localId = i; // give Local id to the future new process
+
+        pid_t result_of_fork = fork();
+
+        // try to create new processes
+        if (result_of_fork == -1) {// fail
+            printf("Can't create new process\n");
+            exit(-1);
+        } else if (result_of_fork == 0) { // we are in child
+            array_of_processes[i].pid = getpid(); // give pid
+            printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
+            // print
+            fprintf(event_log, log_started_fmt, i, getpid(), getppid());
+            fflush(event_log);
+
+
+            Message message = {.s_header = {.s_type = STARTED, .s_magic = MESSAGE_MAGIC},}; // our message, set s_header of Message; set s_type and s_magic of Header
+            sprintf(message.s_payload, log_started_fmt, array_of_processes[i].localId, array_of_processes[i].pid, getppid()); // data of our message in a buffer, set s_payload of Message
+            message.s_header.s_payload_len = (uint16_t) strlen(message.s_payload); // set s_payload_len of Header
+
+            send_multicast(&array_of_processes[i], &message);
+            receive_any(&array_of_processes[i], &message);
+
+            // print
+            printf(log_received_all_started_fmt, i);
+            fprintf(event_log, log_received_all_started_fmt, i);
+            fflush(event_log);
+
+
+
+            // send and receive DONE
+            message.s_header.s_type = DONE;
+            sprintf(message.s_payload, log_done_fmt, array_of_processes[i].localId); // data of our message in a buffer, set s_payload of Message
+            message.s_header.s_payload_len = (uint16_t) strlen(message.s_payload); // set s_payload_len of Header
+            send_multicast(&array_of_processes[i], &message);
+
+            if (i == number_of_processes - 1) {
+                printf(log_received_all_started_fmt, 0);
+                fprintf(event_log, log_received_all_started_fmt, 0);
+                fflush(event_log);
+            }
+
+            // print
+            fprintf(event_log, log_done_fmt, i);
+            fflush(event_log);
+
+            receive_any(&array_of_processes[i], &message);
+            receive_any(&array_of_processes[0], &message); // receive for PARENT
+
+
+            // print
+            printf(log_received_all_done_fmt, i);
+            fprintf(event_log, log_received_all_done_fmt, i);
+            fflush(event_log);
+            if (i == number_of_processes - 1) {
+                printf(log_received_all_done_fmt, 0);
+                fprintf(event_log, log_received_all_started_fmt, 0);
+                fflush(event_log);
+            }
+
+
+            fclose(event_log);
+            exit(0);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc == 3 && strcmp("-p", argv[1]) == 0) { // reading input parameters
+        number_of_processes = atoi(argv[2]);
+    } else {
+        printf("Wrong arguments!\n");
+        exit(-1);
+    }
+
+    if (number_of_processes < 1 || number_of_processes > 10) { // checking number of processes
+        printf("Wrong number of processes!\n");
+        exit(-1);
+    }
+
+    process array_of_processes[++number_of_processes]; // put in an array, 0 process is a main parent process
+
+    int fd[2]; // for standard pipe
+
+    for (int i = 0; i < number_of_processes; i++) { // initialize our arrays
+        array_of_processes[i].pipe_read = (int *) malloc(sizeof(int) * number_of_processes);
+        array_of_processes[i].pipe_write = (int *) malloc(sizeof(int) * number_of_processes);
+    }
+
+    printf("Number of processes = %d\n", number_of_processes);
+    create_pipes(array_of_processes, fd); // our function for creating all pipes
+
+    array_of_processes[0].localId = PARENT_ID; // for parent process
+    array_of_processes[0].pid = getpid(); // for parent process, get pid for current process
+
+
+    create_processes(array_of_processes);
+    sleep(1);
+
+    for (local_id j = 1; j < number_of_processes; ++j) {
+        wait(NULL);
+    }
+
+    close_all_pipes(array_of_processes);
+}
+
+
