7c1bcad50b2bf4f649f8a2a7524f60675f3e16ff
AleksKitty <alexandra.tochansky@gmail.com>
2020-06-21 00:08:28 +0300

    nice printing :-)

--- log.c
+++ log.c
@@ -10,7 +10,7 @@
 void lg (pid_t p, const char * f, const char * m, ...){
     va_list args;
     va_start(args, m); // for reading arg
-    printf("p:%d\t\tf:%s\t\tm:", p, f);
+    printf("p:%d\t\tf:%-14s\t\tm:", p, f);
     vprintf(m, args);
     printf("\n");
     va_end(args);
